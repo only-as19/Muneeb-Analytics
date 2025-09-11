35a7a00a4e67a88aade639a44e543c4ef0311923
Asad <hellohello51981@gmail.com>
2025-09-11 21:06:11 +0500

    Add the tsx for footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,62 @@
+import { logo, SocialLinks, Tools, footerSection, services } from "./data";
+import { Link } from "react-router-dom";
+import { Button } from "@/lib";
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer>
+      <div>
+        <div>
+          <img src={logo} alt="Muneeb Analytics" />
+          <p>Welcome to Muneeb Analyitc</p>
+          <div>
+            {SocialLinks.map((social) => (
+              <div key={social.label}>
+                <Button asChild variant="secondary" className="rounded-full">
+                  <Link to={social.link}>{social.icon}</Link>
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          {footerSection.map((section) => (
+            <div key={section.title}>
+              <h3>{section.title}</h3>
+              <ul>
+                {section.links.map((links) => (
+                  <li key={links.label}>
+                    <Link to={links.link}>{links.label}</Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h3>Services</h3>
 
-export default Footer
+          <ul>
+            {services.map((service) => (
+              <li key={service.label}>
+                <Link to={service.link}>{service.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h3>Tools</h3>
+          <ul>
+            {Tools.map(tool=>(
+              <li key={tool.label}>
+                {tool.label}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
