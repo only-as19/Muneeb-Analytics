2a10fdbfc569d033738fe6d346aa88bc60fdfd4d
Asad <hellohello51981@gmail.com>
2025-09-15 23:28:36 +0500

    feat: Complete about section with responsiveness

--- src/pages/Landing/Sections/About.tsx
+++ src/pages/Landing/Sections/About.tsx
@@ -37,36 +37,41 @@ const About: React.FC = () => {
   return (
     <section className="p-5 max-h-screen min-h-96 flex-col items-center justify-center bg-primary text-zinc-300">
       <div className="max-w-5xl mx-auto flex flex-col gap-y-4">
-        
         <div className="flex flex-col gap-y-6 md:flex-row">
           <div className="text-zinc-300 text-center md:w-1/2 md:text-left flex flex-col justify-center gap-y-4 p-5 md:leading-7 ">
-          <h2 className="text-3xl md:text-5xl font-bold text-center md:text-left">About Muneeb Analytics</h2>
-            <p className="text-sm md:text-base">
+            <h2 className="text-3xl md:text-4xl font-bold text-white text-center md:text-left">
+              About Muneeb Analytics
+            </h2>
+            <p className="text-sm ">
               With expertise in Power BI, Tableau, Excel, SQL, and Python, we
               design dashboards and analytics solutions that help businesses
               track performance, cut costs, and discover new opportunities.
             </p>
-            <p className="text-sm md:text-base">
+            <p className="text-sm">
               Our focus is simple: clarity, accuracy, and results. Whether you
               need to understand your customers, optimize operations, or boost
               profitability, we deliver solutions that empower you to make
               confident decisions.
             </p>
           </div>
-          <div className="md:w-1/2 flex justify-center items-center" >
+          <div className="md:w-1/2 flex justify-center items-center">
             <div className="w-full md:max-w-4/5">
               <Video options={videoJsOptions} onReady={handlePlayerReady} />
             </div>
           </div>
         </div>
         <div className="grid grid-cols-3">
-            {statsData.map(stat=>(
-                <div key={stat.label}
-                className="p-1 rounded-sm shadow-primary text-center flex flex-col items-center justify-between gap-y-1 group">
-                    <p className="text-3xl font-bold text-[#EBF4F6] group-hover:text-[#E8B904]">{stat.value}</p>
-                    <p className="text-zinc-400 text-sm">{stat.label}</p>
-                </div>
-            ))}
+          {statsData.map((stat) => (
+            <div
+              key={stat.label}
+              className="p-1 rounded-sm shadow-primary text-center flex flex-col items-center justify-between gap-y-1 group"
+            >
+              <p className="text-3xl font-bold text-[#EBF4F6] group-hover:text-[#E8B904]">
+                {stat.value}
+              </p>
+              <p className="text-zinc-400 text-sm">{stat.label}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
