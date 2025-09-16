eb7afd59e82de09f499b0f2df78db869e67fce41
Asad <hellohello51981@gmail.com>
2025-09-16 11:31:37 +0500

    feat: Add the tsx data for reviews comonent

--- src/components/Reviews/index.tsx
+++ src/components/Reviews/index.tsx
@@ -4,10 +4,15 @@ import { EffectCoverflow, Pagination } from "swiper/modules";
 import { reviews } from "./data";
 import {SiFiverr} from "react-icons/si"
 import Rating from "../Rating";
-export default function App() {
+import { Navigate, useNavigate } from "react-router-dom";
+import { Button } from "../ui";
+
+const Reviews = ()=> {
+  const navigate = useNavigate()
   return (
-    <>
-      <Swiper
+      <section className="h-screen">
+        <h1>Trusted by Professionals Like You</h1>
+        <Swiper
         effect={"coverflow"}
         grabCursor={true}
         centeredSlides={true}
@@ -33,11 +38,18 @@ export default function App() {
         <SwiperSlide>
           <div>
             <div>
-              <Rating/>
+              <p>{reviews[0].name} <span>{reviews[0].country}</span></p>
+              <Rating />
             </div>
+              <p>{reviews[0].feedback}</p>
+              <div>
+                <Button label="Read on Fiverr"/>
+              </div>
           </div>
         </SwiperSlide>
       </Swiper>
-    </>
+      </section>
   );
 }
+
+export default Reviews
