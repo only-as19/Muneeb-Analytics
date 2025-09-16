22b7a24d39350e6d2ab1ddfd52879d59bdc38186
Asad <hellohello51981@gmail.com>
2025-09-16 10:51:19 +0500

    feat: Set up the swiper for slides

--- src/components/Reviews/index.tsx
+++ src/components/Reviews/index.tsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-const Review:React.FC = () => {
+import "swiper/swiper-bundle.css"
+
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+export default function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="w-full"
+        breakpoints={{
+          0:{
+            slidesPerView:1
+          },
+          768:{
+            slidesPerView:2
+          }
+        }}
+      >
+        <SwiperSlide className=''>
+          <div className='h-36 w-36'>Asad</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='h-36 w-36'>Asad</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='h-36 w-36'>Asad</div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className='h-36 w-36'>Asad</div>
+        </SwiperSlide>
+       
+      </Swiper>
+    </>
+  );
 }
-
-export default Review
