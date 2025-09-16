3b76fdc895845517605cca865c0c156249bdcc10
Asad <hellohello51981@gmail.com>
2025-09-16 09:25:10 +0500

    feat: Write review data

---
+++ src/components/Reviews/data.ts
@@ -0,0 +1,116 @@
+export interface Review {
+  name: string;
+  country: string;
+  feedback: string;
+}
+
+export const reviews: Review[] = [
+  {
+    name: "Letizia De Sario",
+    country: "United Kingdom",
+    feedback:
+      "Muneeb Nasir DELIVERED top-notch work in data visualization, showcasing exceptional professionalism and keen attention to detail. His in-depth understanding, coupled with quick responsiveness and cooperation, made the collaboration seamless. Highly impressed with his ability to execute complex DAX for Power BI process optimisation. I strongly recommend Muneeb!",
+  },
+  {
+    name: "Chris Percival",
+    country: "USA",
+    feedback:
+      "Muneeeb was excellent to work with. Very knowledgeable, patient, and timely. Supported me in SQL which pivoted to also supporting me in by generating leads for my business. The leads were very targeted, and way beyond what I expected in terms of being warm / hot which resulted in a significant increase in my conversion rate. Highly recommended!!!",
+  },
+  {
+    name: "Ahmad",
+    country: "USA",
+    feedback:
+      "I am an HR Product Owner for an educational institute in Silicon Valley. I find Muneeb to be extremely knowledgeable and a true Subject Matter Expert when it comes to the PowerBI tool. He has great communication skills and works with high integrity. I have done several gigs with him and will continue to do so in the future as well. Highly Recommended!",
+  },
+  {
+    name: "Rei Young",
+    country: "Singapore",
+    feedback:
+      "Muneeb provided excellent quality work as always and help me point out errors I’ve failed to see. As always I highly recommend his service to everyone looking for a Power BI expert.",
+  },
+  {
+    name: "Waziha",
+    country: "USA",
+    feedback:
+      "Muneeb explained Power BI concepts clearly and made learning easy. He broke down complex ideas into simple steps and used examples that helped me understand visualizations and reporting. He was patient, approachable, and always ready to answer questions. Highly recommend!",
+  },
+  {
+    name: "Hima",
+    country: "Canada",
+    feedback:
+      "Amazing work ethic, dedication to work and efforts makes him superstar at what Muneeb does!! If he encounters any difficulty, he will work with you to get it fixed and work through it in most perfect way!! Delivered before time even though he was busier. Most recommended!!",
+  },
+  {
+    name: "Andy Parkin",
+    country: "USA",
+    feedback:
+      "An exceptional professional who is great to work with and certainly will be again in the near future. Muneeb is highly recommended !!!!!",
+  },
+  {
+    name: "Ishaan",
+    country: "Canada",
+    feedback:
+      "I’ve put in many orders with Muneeb and he’s always extremely professional & knowledgeable. Would recommend for anything Power BI related.",
+  },
+  {
+    name: "Saeed",
+    country: "USA",
+    feedback:
+      "Muneeb did an outstanding job! He was professional, fast, and delivered exactly what I asked for. Highly recommended and I will definitely work with him again!",
+  },
+  {
+    name: "Petro",
+    country: "Germany",
+    feedback:
+      "Muneeb Nasir is incredibly polite, insightful, helpful, patient, and a genuinely friendly person. Their guidance not only improved my understanding of Power BI but also made the learning process enjoyable and engaging. I highly appreciate their expertise and positive attitude throughout our sessions.",
+  },
+  {
+    name: "Atkins",
+    country: "Malaysia",
+    feedback:
+      "Really happy to work with Muneeb again as he really goes above and beyond to meet my expectations. Thank you!",
+  },
+  {
+    name: "Muhammad Umair",
+    country: "India",
+    feedback:
+      "Muneeb is a thorough professional with a deep understanding of his domain. Problem solver, polite, good listener, and communicator.",
+  },
+  {
+    name: "Shoaib Karmat",
+    country: "Australia",
+    feedback:
+      "Muneeb was keen to understand the project deliverables and went above and beyond that the project was delivered on-time professionally. Five Star :)",
+  },
+  {
+    name: "Rachel",
+    country: "USA",
+    feedback:
+      "Professional, responsive, and delivered exceptional results. Thank you for your hard work!",
+  },
+  {
+    name: "Opeyemi",
+    country: "USA",
+    feedback:
+      "Muneeb went above and beyond, he is such a good sport, he was calm and explained in an understanding manner. I will always work with him as he is a good tutor with a good spirit. I will recommend him any day any time.",
+  },
+  {
+    name: "Frances Downey",
+    country: "USA",
+    feedback:
+      "Awesome to work with and awesome work! The video chats really help dial in my project. Highly recommend!",
+  },
+  {
+    name: "Usama",
+    country: "United Arab Emirates",
+    feedback:
+      "Great guy definitely going to hire him again for my future projects. He has very good knowledge of Data analytics and his cooperation is very good. I definitely recommend him.",
+  },
+  {
+    name: "Antonio",
+    country: "USA",
+    feedback:
+      "Great understanding of complicated task, will use again.",
+  },
+];
