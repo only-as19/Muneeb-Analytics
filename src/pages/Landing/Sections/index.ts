2a98d4e6092b718e5386c249075134dbee8598e9
Asad <hellohello51981@gmail.com>
2025-09-18 09:33:38 +0500

    feat: Set up Swiper slider

--- src/pages/Landing/Sections/index.ts
+++ src/pages/Landing/Sections/index.ts
@@ -1,3 +1,3 @@
 export {default as Hero} from "./hero"
 export {default as About} from "./About"
-export {default as Services} from "./About"
+export {default as Services} from "./services"
--- src/pages/Landing/Sections/services.tsx
+++ src/pages/Landing/Sections/services.tsx
@@ -1,5 +1,34 @@
-const services: React.FC = () => {
-  return <div>Services</div>;
+import { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { Button } from "@/components";
+
+const Services: React.FC = () => {
+  const [swiper, setSwiper] = useState<any>(null);
+
+  return (
+    <section>
+      <div>
+        <Swiper
+          modules={[Navigation]}
+          slidesPerView={2}
+          loop={true}
+          onSwiper={setSwiper} // capture swiper instance
+        >
+          <SwiperSlide className="bg-primary text-white">1</SwiperSlide>
+          <SwiperSlide className="bg-primary text-white">2</SwiperSlide>
+          <SwiperSlide className="bg-primary text-white">3</SwiperSlide>
+          <SwiperSlide className="bg-primary text-white">4</SwiperSlide>
+        </Swiper>
+
+        {/* external button */}
+        <Button label="Prev" onClick={() => swiper?.slidePrev()} />
+        <Button label="Prev" onClick={() => swiper?.slideNext()} />
+      </div>
+    </section>
+  );
 };
 
-export default services;
+export default Services;
