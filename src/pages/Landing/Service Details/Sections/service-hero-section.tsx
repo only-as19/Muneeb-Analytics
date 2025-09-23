caa31c2a34e146b933492a6f88ce5f7e005149c0
Asad <hellohello51981@gmail.com>
2025-09-23 11:36:10 +0500

    feat: a

--- src/pages/Landing/Service Details/Sections/service-hero-section.tsx
+++ src/pages/Landing/Service Details/Sections/service-hero-section.tsx
@@ -1,25 +1,20 @@
 import { Button } from "@/components";
 import { useParams } from "react-router";
 import { Services } from "../../data";
-const ServiceHero = () => {
+const ServiceHero:React.FC = () => {
   const { serviceId } = useParams();
-  const serviceContent = Services.filter(
+  const serviceContent = Services.find(
     (service) => service.link === serviceId
   );
-  const data = serviceContent.map(service=> service.powerbi)
-  console.log(data)
-
-  console.log(serviceContent)
+  const hero = serviceContent?.powerbi?.hero
+  if (!hero) return <p>Service not found</p>;
+  
   return (
     <section>
       <div>
-        <h1>Unlock Real&ndash;Time Business Intelligence</h1>
+        <h1>{hero.subtitle}</h1>
         <p>
-          Turn raw data into powerful insights with fully automated dashboards
-          designed for Sales, Finance, Marketing, HR, Supply Chain, Operations,
-          Customer Service, IT, and Project Management. Gain a complete view of
-          your organization in real time so you can track performance, uncover
-          oppertunities, and make smarter decesions with confidence.
+          {hero.description}
         </p>
         <Button label="Pricing" />
       </div>
