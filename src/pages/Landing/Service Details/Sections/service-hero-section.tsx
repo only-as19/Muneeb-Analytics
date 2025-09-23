1e5dc4ed4b90953355fe086b3257e94dab14d695
Asad <hellohello51981@gmail.com>
2025-09-23 12:20:48 +0500

    feat: style the headig section and make responsive

--- src/pages/Landing/Service Details/Sections/service-hero-section.tsx
+++ src/pages/Landing/Service Details/Sections/service-hero-section.tsx
@@ -10,13 +10,20 @@ const ServiceHero:React.FC = () => {
   if (!hero) return <p>Service not found</p>;
   
   return (
-    <section>
-      <div>
-        <h1>{hero.subtitle}</h1>
-        <p>
+    <section className="md:min-h-screen min-h-96 bg-bg-secondary p-5 flex flex-col justify-center items-center">
+      <div className="max-w-6xl mx-auto">
+        <div className="md:w-1/2">
+          <h1 className="text-3xl md:text-4xl font-bold text-primary mb-2">{hero.title}</h1>
+        <h3 className="text-xl md:text-2xl mb-1">{hero.subtitle}</h3>
+        <p className="text-muted-foreground mb-6">
           {hero.description}
         </p>
-        <Button label="Pricing" />
+        <div className="flex justify-center md:justify-start gap-x-2">
+          <Button label="Pricing List" className=""/>
+        <Button label="Pricing List" variant="outline" className=""/>
+        </div>
+        </div>
+        
       </div>
     </section>
   );
