d6f1a45d8a3f839ac617dad83a0fe30006798a3d
Asad <hellohello51981@gmail.com>
2025-09-30 00:33:52 +0500

    feat: Add the Pricing Card Component

--- src/components/PricingCard/index.tsx
+++ src/components/PricingCard/index.tsx
@@ -6,12 +6,50 @@ import {
   CardFooter,
   CardDescription,
 } from "@/lib";
+import type { ReactNode } from "react";
+import { Button } from "../ui";
 
-const Card = () => {
+interface cardProps {
+  price: string;
+  name: string;
+  isPopular?: boolean;
+  children: ReactNode;
+  className?: string;
+  description: string;
+  buttonLabel: string;
+  buttonVariant:"link" | "default" | "outline" | "secondary" | "destructive" | "ghost"
+}
+
+const Card: React.FC<cardProps> = ({
+  price,
+  name,
+  isPopular,
+  children,
+  className,
+  description,
+  buttonLabel,
+  buttonVariant = "default"
+}) => {
   return (
-  <PricingCard>
-    
-  </PricingCard>
+    <PricingCard className={`${className}`}>
+      <CardHeader>
+        <div>
+          <CardTitle>{name}</CardTitle>
+          {isPopular && (
+            <span>Most Popular</span>
+          )}
+        </div>
+        <h1>{price}</h1>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        {children}
+      </CardContent>
+      <CardFooter>
+          <Button label={buttonLabel} variant={buttonVariant}/>
+      </CardFooter>
+      
+    </PricingCard>
   );
 };
 
