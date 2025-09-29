b834859ba1ff87363a125dd5d7abf8baea498df7
Asad <hellohello51981@gmail.com>
2025-09-29 20:55:20 +0500

    feat: Style the feature cards

--- src/pages/Landing/Service Details/Sections/serviceFeature.tsx
+++ src/pages/Landing/Service Details/Sections/serviceFeature.tsx
@@ -1,20 +1,26 @@
 import { features } from "../../data";
 const ServiceFeature: React.FC = () => {
   return (
-    <section>
-      <div>
+    <section className="p-5 min-h-96 bg-bg-secondary text-primary flex flex-col justify-center items-center">
+      <div className="max-w-6xl mx-auto grid gap-y-4">
         {features.map((feature, index) => (
-          <div key={index}>
-            <div>
-              <div>{feature.icon}</div>
-              <div>
-                <h1>{feature.metrices}</h1>
-                <span>{feature.metricesLabel}</span>
+          <div key={index}
+          className="bg-white shadow-primary shadow-2xl px-6 py-8 rounded-md flex flex-col gap-y-4"
+          >
+            <div className="flex justify-between items-center">
+              <div className={`h-14 w-14 text-white flex justify-center items-center rounded-full`}
+              style={{backgroundColor: feature.color}}
+              >{feature.icon}</div>
+              <div className="text-right">
+                <h1 className="text-2xl font-bold"
+                style={{color:feature.color}}
+                >{feature.metrices}</h1>
+                <span className="text-sm text-muted-foreground">{feature.metricesLabel}</span>
               </div>
             </div>
             <div>
-              <h1>{feature.title}</h1>
-              <p>{feature.description}</p>
+              <h1 className="text-2xl font-bold">{feature.title}</h1>
+              <p className="text-muted-foreground">{feature.description}</p>
             </div>
           </div>
         ))}
