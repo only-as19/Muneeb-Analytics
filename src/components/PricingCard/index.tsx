fd9950e36bdd09b6c51771f001fd0555c6c12c47
Asad <hellohello51981@gmail.com>
2025-09-30 12:58:42 +0500

    feat: Style pricing cards

--- src/components/PricingCard/index.tsx
+++ src/components/PricingCard/index.tsx
@@ -14,8 +14,8 @@ interface cardProps {
     price: string;
     name: string;
     description: string;
-    delivery:ReactNode
-    revesion:ReactNode
+    delivery: ReactNode;
+    revesion: ReactNode;
   };
   isPopular?: boolean;
   children: ReactNode;
@@ -41,21 +41,25 @@ const Card: React.FC<cardProps> = ({
 }) => {
   return (
     <PricingCard className={`${className}`}>
-      <CardHeader>
-        <div>
-          <CardTitle>{cardHeader.name}</CardTitle>
-          {isPopular && <span>Most Popular</span>}
+      <CardHeader className="gap-y-4">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-2xl">{cardHeader.name}</CardTitle>
+          {isPopular && <span className="px-3 py-1 rounded-4xl shadow">Most Popular</span>}
         </div>
-        <h1>{cardHeader.price}</h1>
+        <h1 className="text-5xl">${cardHeader.price}</h1>
         <CardDescription>{cardHeader.description}</CardDescription>
-        <div>
-          <span>{cardHeader.delivery}</span>
-          <span>{cardHeader.revesion}</span>
+        <div className="flex gap-x-6 items-center">
+          <span className="flex items-center gap-x-2 font-bold text-sm">{cardHeader.delivery}</span>
+          <span className="flex items-center gap-x-2 font-bold text-sm">{cardHeader.revesion}</span>
         </div>
       </CardHeader>
       <CardContent>{children}</CardContent>
       <CardFooter>
-        <Button label={footer.buttonLabel} variant={footer.buttonVariant} />
+        <Button
+          label={footer.buttonLabel}
+          variant={footer.buttonVariant}
+          customClasses="w-full shadow cursor-pointer"
+        />
       </CardFooter>
     </PricingCard>
   );
--- src/pages/Landing/Service Details/Sections/pricing.tsx
+++ src/pages/Landing/Service Details/Sections/pricing.tsx
@@ -1,51 +1,78 @@
-import { PricingData} from "../../data"
-import { PricingCard } from "@/components"
-import { FaCheckCircle } from "react-icons/fa";
-import { Clock3, RefreshCcw } from "lucide-react";
+import { PricingData } from "../../data";
+import { PricingCard } from "@/components";
+import { Clock3, RefreshCcw,CircleCheckBig } from "lucide-react";
 
-
-
-const Pricing:React.FC = () => {
+const Pricing: React.FC = () => {
   return (
-    <section className="p-5 min-h-screen flex flex-col  items-center bg-secondary">
+    <section className="p-5 min-h-screen flex flex-col color-for items-center bg-secondary font-roboto">
+      <div>
         <div>
-            <div>
-                <h1>Lorem ipsum dolor sit.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid necessitatibus hic quas? Minus accusamus vitae cum autem, expedita qui illo error quas blanditiis officiis, quibusdam assumenda eum voluptas dolores explicabo?</p>
-            </div>
+          <h1>Lorem ipsum dolor sit.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            necessitatibus hic quas? Minus accusamus vitae cum autem, expedita
+            qui illo error quas blanditiis officiis, quibusdam assumenda eum
+            voluptas dolores explicabo?
+          </p>
         </div>
-        <div>
-            {PricingData.map(pricing =>(
-                <div key={pricing.name}>
-                    <PricingCard
-                    cardHeader={{
-                        name:pricing.name,
-                        price:pricing.price,
-                        description:pricing.description,
-                        delivery:<><Clock3/> <span>{pricing.delivery}</span> </>,
-                        revesion:<><RefreshCcw/> <span>{pricing.revesion}</span> </>
-                    }}
-                    footer={{
-                        buttonLabel:"Contact",
-                        buttonVariant:"default"
-                    }}
-                    >
-                        <div>
-                            <ul>
-                                {pricing.feature.map((list,index)=>(
-                                    <li key={index}>
-                                        <FaCheckCircle /> <span>{list}</span>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </PricingCard>
+      </div>
+      <div>
+        {PricingData.map((pricing) => (
+          <div key={pricing.name}>
+            <PricingCard
+              cardHeader={{
+                name: pricing.name,
+                price: pricing.price,
+                description: pricing.description,
+                delivery: (
+                  <>
+                    <Clock3 size={20} strokeWidth={3} />{" "}
+                    <span>{pricing.delivery}</span>{" "}
+                  </>
+                ),
+                revesion: (
+                  <>
+                    <RefreshCcw size={20} strokeWidth={3} />{" "}
+                    <span>{pricing.revesion}</span>{" "}
+                  </>
+                ),
+              }}
+              isPopular={pricing.isPopular}
+              footer={{
+                buttonLabel: "Contact",
+                buttonVariant: "default",
+              }}
+            >
+              <div className="flex flex-col gap-y-4">
+                <div className="flex items-center justify-center">
+                  <div className="flex items-center flex-1">
+                    <span className="w-1 h-1 rounded-full bg-gray-500"></span>
+                    <div className="flex-1 border-t border-gray-600 mx-2"></div>
+                  </div>
+                  <span className="px-3 text-xs uppercase tracking-widest text-gray-400">
+                    Features
+                  </span>
+                  <div className="flex items-center flex-1">
+                    <div className="flex-1 border-t border-gray-600 mx-2"></div>
+                    <span className="w-1 h-1 rounded-full bg-gray-500"></span>
+                  </div>
                 </div>
-            ))}
-        </div>
+                <ul className="flex flex-col gap-y-3">
+                  {pricing.feature.map((list, index) => (
+                    <li key={index}
+                    className="flex items-start gap-x-3"
+                    >
+                      <CircleCheckBig size={20} className=" shrink-0" /> <span className="text-sm">{list}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </PricingCard>
+          </div>
+        ))}
+      </div>
     </section>
-      
-  )
-}
+  );
+};
 
-export default Pricing
+export default Pricing;
