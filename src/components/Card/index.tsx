5f76e3dd7bb3b9f94ea459aa935a15aade459e00
Asad <hellohello51981@gmail.com>
2025-10-02 01:01:33 +0500

    refactor: Update Service Card Design

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,73 +10,71 @@ import {
 import type { ReactNode } from "react";
 import { Button } from "@/components";
 import { useNavigate } from "react-router";
-interface cardProps{
-    headerImg: string,
-    children: ReactNode,
-    className?: string,
-    cardTitle: string,
-    cardDescription: string,
-    footer:{
-        buttonLabel:string
-        buttonLink:string
-    }
+interface cardProps {
+  headerImg: string;
+  children: ReactNode;
+  className?: string;
+  cardTitle: string;
+  cardDescription: string;
+  footer: {
+    buttonLabel?: string;
+    buttonLink: string;
+    btnClass?:string
+    icon?:ReactNode
+    variant?:"ghost" | "link" | "outline" | "default" | "secondary" | "destructive"
+  };
 }
 
-const Card:React.FC<cardProps> = ({
-    headerImg,
-    className,
-    children,
-    cardTitle,
-    cardDescription,
-    footer,
-    ...props
+const Card: React.FC<cardProps> = ({
+  headerImg,
+  className,
+  children,
+  cardTitle,
+  cardDescription,
+  footer,
+  ...props
 }) => {
-
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
-    <ShadcnCard className={`${className} h-full w-full overflow-hidden rounded p-0 gap-y-4`}
-    {...props}
+    <ShadcnCard
+      className={`${className} h-full w-full overflow-hidden rounded p-0 gap-y-4`}
+      {...props}
     >
-                  <CardHeader
-                    className="
-                      p-0 relative overflow-hidden shrink-0
-                      w-full h-56 md:h-50
-                      rounded-b-none
-                    "
-                  >
-                    {headerImg && (
-                        <img
-                      loading="lazy"
-                      src={headerImg}
-                      alt={cardTitle}
-                      className="absolute inset-0 h-full w-full object-cover block"
-                    />
-                    )}
-                    
-                  </CardHeader>
-
-                  <CardContent className="flex-1">
-                    <CardTitle>
-                      <h2 className="line-clamp-1 text-xl font-semibold text-primary mb-2">
-                        {cardTitle}
-                      </h2>
-                    </CardTitle>
-                    <CardDescription className="line-clamp-2 text-destructive">
-                      {cardDescription}
-                    </CardDescription>
-                    {children}
-                  </CardContent>
-
-                  <CardFooter className="mt-auto pb-5 flex flex-end">
-                    <CardAction className="w-full ml-auto">
-                      <Button label={footer.buttonLabel} customClasses="w-full cursor-pointer" 
-                      onClick={()=> navigate(`${footer.buttonLink}`)}
-                      />
-                    </CardAction>
-                  </CardFooter>
+      <CardHeader className="p-0 relative overflow-hidden shrink-0 w-full h-56 md:h-50 rounded-b-none">
+        {headerImg && (
+          <img
+            src={headerImg}
+            loading="lazy"
+            className="absolute inset-0 h-full w-full object-cover block"
+            alt={cardTitle}
+          />
+        )}
+      </CardHeader>
+      <CardContent className="flex-1">
+        <CardTitle>
+          <h2 className="line-clamp-1 text-xl font-semibold text-primary mb-2">
+            {cardTitle}
+          </h2>
+        </CardTitle>
+        <CardDescription className="line-clamp-2 text-destructive">
+          {cardDescription}
+        </CardDescription>
+        {children}
+      </CardContent>
+      <CardFooter className="mt-auto pb-5 flex flex-end">
+        <CardAction className="ml-auto">
+          <Button
+          variant={footer.variant}
+          icon={footer.icon}
+          label={footer.buttonLabel}
+          customClasses={`${footer} cursor-pointer`}
+          onClick={() => navigate(`${footer.buttonLink}`)}
+          />
+        </CardAction>
+      </CardFooter>
     </ShadcnCard>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/pages/Landing/Sections/services.tsx
+++ src/pages/Landing/Sections/services.tsx
@@ -7,20 +7,20 @@ import type { Swiper as SwiperType } from "swiper";
 import { motion } from "motion/react";
 import { Button } from "@/components";
 import { Services } from "../data";
-import { ArrowLeft, ArrowRight } from "lucide-react";
-import {Card, Rating} from "@/components";
+import { ArrowLeft, ArrowRight, Video, TrendingUp } from "lucide-react";
+import { Card, Rating } from "@/components";
 
-const Service:React.FC = () => {
+const Service: React.FC = () => {
   const [swiper, setSwiper] = useState<SwiperType | null>(null);
-  return(
+  return (
     <motion.section
-      className="relative p-5 min-h-screen bg-primary md:bg-secondary flex items-center"
+      className="relative p-5 min-h-screen bg-secondary flex items-center font-roboto shadow-2xl shadow-primary"
       viewport={{ amount: 0.4, once: false }}
       onViewportEnter={() => swiper?.autoplay.start()}
       onViewportLeave={() => swiper?.autoplay.stop()}
     >
       <div className="md:max-w-6xl mx-auto w-full">
-        <h1 className="uppercase text-white md:text-primary text-3xl text-center mb-8 font-bold md:text-4xl">
+        <h1 className="uppercase text-foreground text-3xl text-center mb-8 font-bold md:text-4xl">
           Services I offer
         </h1>
 
@@ -44,16 +44,43 @@ const Service:React.FC = () => {
             {Services.map((service) => (
               <SwiperSlide key={service.label} className="flex">
                 <Card
-                headerImg={service.img || ""}
-                cardTitle={service.label}
-                cardDescription={service.description}
-                footer={{
-                  buttonLabel: "Check Details",
-                  buttonLink:service.link || ""
-                }}
+                  headerImg={service.img || ""}
+                  cardTitle={service.label}
+                  cardDescription={service.description}
+                  footer={{
+                    buttonLink: service.link || "",
+                    icon:<TrendingUp className="!h-6 !w-7"/>,
+                    btnClass:"self-end",
+                    variant:"ghost"
+                  }}
                 >
-                  <div>
-                    <Rating rating={4.5} star={4.3} size={15} review={40}/>
+                  <div className="flex flex-col gap-y-1 mt-1">
+                    <div className="flex items-center justify-between">
+                      <Rating rating={4.5} star={4.3} size={15} review={40} />
+                      <span className="bg-orange-400 text-white text-xs px-2 py-1 rounded">
+                        Top rated
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center">
+                      <span className="text-lg font-semibold">
+                        From $400-$800
+                      </span>
+                      <p className="text-sm">
+                        Duration
+                        <span className="block text-xs text-muted-foreground">
+                          7-30 Days
+                        </span>
+                      </p>
+                    </div>
+                    <p className="flex items-center gap-x-2 text-muted-foreground text-sm">
+                      <Video size={20} /> Offer video consultaion
+                    </p>
+                    <p className="inline-block text-xs mt-2 text-white  ">
+                      <span className="rounded standard px-3 py-1">
+                        Most selled
+                      </span>
+                    </p>
                   </div>
                 </Card>
               </SwiperSlide>
