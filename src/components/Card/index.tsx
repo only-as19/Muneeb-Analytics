2187b129395df460b182a63d93ef701fc29b327e
Asad <hellohello51981@gmail.com>
2025-10-01 15:44:58 +0500

    refactor: Added New Card Component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -7,11 +7,72 @@ import {
   CardDescription,
   CardContent,
 } from "@/lib";
+import type { ReactNode } from "react";
+import { Button } from "@/components";
+import { useNavigate } from "react-router";
+interface cardProps{
+    headerImg: string,
+    children: ReactNode,
+    className?: string,
+    cardTitle: string,
+    cardDescription: string,
+    footer:{
+        buttonLabel: string,
+        footerLink: string
+    }
+}
 
-const Card = () => {
+const Card:React.FC<cardProps> = ({
+    headerImg,
+    className,
+    children,
+    cardTitle,
+    cardDescription,
+    footer,
+    ...props
+}) => {
+
+    const navigate = useNavigate()
+
   return (
-    <ShadcnCard>
-        
+    <ShadcnCard className={`${className} h-full w-full overflow-hidden rounded p-0 gap-y-4`}
+    {...props}
+    >
+                  <CardHeader
+                    className="
+                      p-0 relative overflow-hidden shrink-0
+                      w-full h-56 md:h-50
+                      rounded-b-none
+                    "
+                  >
+                    <img
+                      loading="lazy"
+                      src={headerImg}
+                      alt={cardTitle}
+                      className="absolute inset-0 h-full w-full object-cover block"
+                      
+                    />
+                  </CardHeader>
+
+                  <CardContent className="flex-1">
+                    <CardTitle>
+                      <h2 className="line-clamp-1 text-xl font-semibold text-primary mb-2">
+                        {cardTitle}
+                      </h2>
+                    </CardTitle>
+                    <CardDescription className="line-clamp-2 text-destructive">
+                      {cardDescription}
+                    </CardDescription>
+                    {children}
+                  </CardContent>
+
+                  <CardFooter className="mt-auto pb-5 flex flex-end">
+                    <CardAction className="w-full ml-auto">
+                      <Button label={footer.buttonLabel} customClasses="w-full cursor-pointer" 
+                      onClick={()=> navigate(`${footer.footerLink}`)}
+                      />
+                    </CardAction>
+                  </CardFooter>
     </ShadcnCard>
   )
 }
