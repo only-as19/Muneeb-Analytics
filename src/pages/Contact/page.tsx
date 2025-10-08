58eee6588ce17fd7cea462e7fdfc60a12b24c0b3
Asad <hellohello51981@gmail.com>
2025-10-08 12:53:11 +0500

    feat: Add basic contact form

--- src/pages/Contact/page.tsx
+++ src/pages/Contact/page.tsx
@@ -2,10 +2,10 @@ import {ContactForm} from "./section"
 
 const Contact = () => {
   return (
-    <section>
-      <div className="bg-secondary flex flex-col justify-center items-center p-5">
+    <section className="bg-secondary">
+      <div className="bg-secondary flex flex-col justify-center items-center px-5 py-12 max-w-6xl mx-auto">
         <h1 className="text-3xl md font-bold">Contact us</h1>
-        <p className="text-muted-foreground text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consectetur cum dolores quod! Necessitatibus animi quos, eius accusantium eum vel.</p>
+        <p className="text-muted-foreground text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consectetur cum dolores quod! Necessitatibus animi quos, eius accusantium eum vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, consequatur?</p>
       </div>
       <ContactForm />
 
--- src/pages/Contact/section/contacForm.tsx
+++ src/pages/Contact/section/contacForm.tsx
@@ -1,10 +1,10 @@
 import { Button, Input } from "@/components"
 import { CircleArrowOutUpRight } from 'lucide-react';const ContacForm = () => {
   return (
-    <section className="min-h-screen p-5">
-        <div className="grid md:grid-cols-2">
-            <div className="flex flex-col gap-y-2 text-center md:text-left">
-                <h1 className="text-2xl font-bold">Lorem, ipsum dolor</h1>
+    <section className="min-h-screen p-5 bg-white">
+        <div className="grid md:grid-cols-2 max-w-6xl mx-auto">
+            <div className="flex flex-col justify-center gap-y-4 text-center md:text-left">
+                <h1 className="md:text-4xl text-2xl font-bold">Lorem, ipsum dolor</h1>
                 <p className="text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur sunt veritatis, maiores necessitatibus nulla quidem!</p>
                 <Input
                 InputType="text"
@@ -18,7 +18,47 @@ import { CircleArrowOutUpRight } from 'lucide-react';const ContacForm = () => {
                     <Button label="xyz"/>
                 </div>
             </div>
-            <div></div>
+            <div>
+                <form className="grid gap-y-4 md:grid-cols-2 md:gap-x-4">
+                    <Input
+                    InputType="text"
+                    label="Name"
+                    placeholder="Enter your name"
+                    />
+                    <Input 
+                    InputType="number"
+                    label="Number"
+                    placeholder="Enter your Number"
+                    />
+                    <Input 
+                    InputType="email"
+                    label="Email Address"
+                    placeholder="Enter your Email"
+                    />
+                    <Input 
+                    InputType="text"
+                    label="Name"
+                    placeholder="Enter name"
+                    />
+                    <select className="border-2">
+                        <option>Services</option>
+                        <option>Service 1</option>
+                        <option>Service 2</option>
+                        <option>Service 3</option>
+                    </select>
+                    <Input 
+                    InputType="number"
+                    label="Budget"
+                    placeholder="What's your budget"
+                    />
+                    <div className="flex flex-col">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" 
+                        className="w-full border rounded-md p-5 h-20"
+                        ></textarea>
+                    </div>
+                </form>
+            </div>
         </div>
     </section>
   )
