fe1543dbfceaf63bb48ddc7b756e0c9187f84543
Asad <hellohello51981@gmail.com>
2025-10-08 22:18:52 +0500

    feat: Solve from layout issue and input style

--- src/components/ui/Input/index.tsx
+++ src/components/ui/Input/index.tsx
@@ -1,17 +1,18 @@
 import {Input as ShadCnInput} from "@/lib"
 import Button from "../button";
-import type { InputHTMLAttributes } from "react"
+import type { InputHTMLAttributes, ReactNode } from "react"
 
 
 interface TInput extends Omit<InputHTMLAttributes<HTMLInputElement>,'type'>{
     label?: string;
     value?: string;
-    InputType:string;
+    InputType?:string;
     customClass?: string;
     button?:{
         buttonLabel?:string;
         buttonVariant?: "default" | "outline" | "secondary" | "destructive" | "ghost" | "link";
-        buttoOnClick?: (value?: any) => void
+        buttoOnClick?: (value?: any) => void;
+        buttonIcon?:ReactNode
     },
 }
 
@@ -27,15 +28,19 @@ const Input:React.FC<TInput> = ({
     ...rest
 }) => {
   return (
-    <div className={`${label ? "grid" : "flex"} w-full max-w-sm items-center`}>
-        {label && <label htmlFor={name}>{label}</label>}
+    <div className={`flex ${label ? "grid" : "flex"} grid-col-1 w-full items-center gap-1`}>
+        {label && <label htmlFor={name}
+        className="font-semibold">{label}</label>}
+        <div>
+          
+        </div>
       <ShadCnInput
         name={name}
         type={InputType}
         value={value}
         placeholder={placeholder}
         onChange={onChange}
-        className={`${customClass}`}
+        className={`${customClass} w-full focus-visible:ring-muted-foreground/10 `}
         {...rest}
       />
       {button && (
--- src/pages/Contact/section/contacForm.tsx
+++ src/pages/Contact/section/contacForm.tsx
@@ -2,8 +2,8 @@ import { Button, Input } from "@/components"
 import { CircleArrowOutUpRight } from 'lucide-react';const ContacForm = () => {
   return (
     <section className="min-h-screen p-5 bg-white">
-        <div className="grid md:grid-cols-2 max-w-6xl mx-auto">
-            <div className="flex flex-col justify-center gap-y-4 text-center md:text-left">
+        <div className="grid md:grid-cols-2 max-w-6xl mx-auto gap-y-4 md:gap-x-4">
+            <div className="flex flex-col justify-center items-center p-10 gap-y-4 text-center md:text-left bg-bg-secondary">
                 <h1 className="md:text-4xl text-2xl font-bold">Lorem, ipsum dolor</h1>
                 <p className="text-muted-foreground">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur sunt veritatis, maiores necessitatibus nulla quidem!</p>
                 <Input
@@ -18,12 +18,12 @@ import { CircleArrowOutUpRight } from 'lucide-react';const ContacForm = () => {
                     <Button label="xyz"/>
                 </div>
             </div>
-            <div>
-                <form className="grid gap-y-4 md:grid-cols-2 md:gap-x-4">
+                <form className="w-full grid gap-y-4 md:grid-cols-2 md:gap-x-2">
                     <Input
                     InputType="text"
                     label="Name"
                     placeholder="Enter your name"
+                    customClass="w-full"
                     />
                     <Input 
                     InputType="number"
@@ -40,26 +40,35 @@ import { CircleArrowOutUpRight } from 'lucide-react';const ContacForm = () => {
                     label="Name"
                     placeholder="Enter name"
                     />
-                    <select className="border-2">
-                        <option>Services</option>
-                        <option>Service 1</option>
-                        <option>Service 2</option>
-                        <option>Service 3</option>
-                    </select>
-                    <Input 
-                    InputType="number"
-                    label="Budget"
-                    placeholder="What's your budget"
-                    />
+                    
                     <div className="flex flex-col">
-                        <label htmlFor="message">Message</label>
-                        <textarea name="message" 
-                        className="w-full border rounded-md p-5 h-20"
+                        <label 
+                        className="font-semibold"
+                        htmlFor="service">Service</label>
+                        <select name="service">
+                            <option value="" disabled>Services</option>
+                            <option value="1">Service1</option>
+                            <option value="2">Service2</option>
+                            <option value="3">Service3</option>
+                        </select>
+                    </div>
+                    <Input 
+                    InputType="text"
+                    label="Budget"
+                    placeholder="Enter your Budget"
+                    />
+                    <div className="flex flex-col gap-y-2 col-span-2">
+                        <label
+                        className="font-semibold"
+                        htmlFor="message">Message</label>
+                        <textarea 
+                        className="h-20 w-full border-2 rounded"
+                        name="message"
                         ></textarea>
                     </div>
+                    <Button type="submit" label="Send Email"/>
                 </form>
             </div>
-        </div>
     </section>
   )
 }
