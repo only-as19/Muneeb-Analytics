9c179b2ab90ed9ec70864cfbe27aa6143e3a6039
Asad <hellohello51981@gmail.com>
2025-10-13 23:40:24 +0500

    feat: Add content of Case studeis

--- src/pages/Projects/page.tsx
+++ src/pages/Projects/page.tsx
@@ -1,8 +1,11 @@
 const Project = () => {
   return (
-    <div>
-        Project
-    </div>
+    <section>
+      <div>
+        <h1>Case Studies</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nam? Quos placeat qui quod laboriosam quo provident nostrum minima. Iure?</p>
+      </div>
+    </section>
   )
 }
 
