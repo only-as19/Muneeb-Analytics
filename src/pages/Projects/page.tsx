1de5dd046f23b85402ea124abc9f7a12ad6c46b3
Asad <hellohello51981@gmail.com>
2025-10-14 21:41:59 +0500

    feat: Remove key error

--- src/pages/Projects/page.tsx
+++ src/pages/Projects/page.tsx
@@ -2,7 +2,6 @@ const Project = () => {
   return (
     <section>
       <div>
-        <h1>Case Studies</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nam? Quos placeat qui quod laboriosam quo provident nostrum minima. Iure?</p>
       </div>
     </section>
