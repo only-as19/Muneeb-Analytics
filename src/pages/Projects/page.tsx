36b7e861451f29134bc3620e4c5f46586f293de1
Asad <hellohello51981@gmail.com>
2025-10-14 23:20:37 +0500

    feat: Style and make rsponsive

--- src/pages/Projects/page.tsx
+++ src/pages/Projects/page.tsx
@@ -4,16 +4,17 @@ import { caseStudies } from "./data";
 const Project: React.FC = () => {
   return (
     <section className="p-5 bg-secondary text-primary">
-      <div>
+      <div className="max-w-6xl mx-auto">
         <div className="">
           <h3 className="text-2xl font-semibold md:text-3xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
             quia quisquam qui at, alias aut voluptas? Eaque a nostrum possimus.
           </h3>
         </div>
-        <div>
+        <div className="my-20 grid gap-6 md:grid-cols-3">
           {caseStudies.map((study) => (
             <Card
+            className="bg-transparent shadow-xs"
               key={study.id}
               headerImg={study.image}
               cardTitle={study.title}
@@ -23,10 +24,10 @@ const Project: React.FC = () => {
                 buttonLink: study.id,
               }}
             >
-              <div>
-                <span>Industry : ??? </span>
-                <p>Duration: <span>???</span></p>
-                <p>{study.impact}</p>
+              <div className="flex flex-col gap-2 mt-2">
+                <span className="text-sm text-muted-foreground">Industry : ??? </span>
+                <p className="text-sm">Duration: <span>???</span></p>
+                <p ><span className="bg-primary/20 text-sm font-semibold text-primary px-2 py-1 rounded-sm ">{study.impact}</span></p>
               </div>
             </Card>
           ))}
