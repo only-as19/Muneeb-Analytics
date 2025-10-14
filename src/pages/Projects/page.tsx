5e7b18e276a5f258c85680c068dbf64f4180f6ff
Asad <hellohello51981@gmail.com>
2025-10-14 22:31:16 +0500

    feat: Added heading, style and make it responsive

--- src/pages/Projects/page.tsx
+++ src/pages/Projects/page.tsx
@@ -1,8 +1,11 @@
-const Project = () => {
+const Project:React.FC = () => {
   return (
-    <section>
+    <section className="p-5 bg-secondary text-primary">
       <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime, nam? Quos placeat qui quod laboriosam quo provident nostrum minima. Iure?</p>
+        <div className="">
+          <h3 className="text-2xl font-semibold md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quia quisquam qui at, alias aut voluptas? Eaque a nostrum possimus.</h3>
+        </div>
+        <div></div>
       </div>
     </section>
   )
