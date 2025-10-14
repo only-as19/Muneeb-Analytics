f681aa4667695b54d1833ec3d6be35ac14e14096
Asad <hellohello51981@gmail.com>
2025-10-14 23:04:06 +0500

    feat: Added content for cards

--- src/pages/Projects/page.tsx
+++ src/pages/Projects/page.tsx
@@ -1,22 +1,39 @@
-import { Card } from "@/components"
-import { caseStudies } from "./data"
-const Project:React.FC = () => {
+import { Card } from "@/components";
+import { caseStudies } from "./data";
+
+const Project: React.FC = () => {
   return (
     <section className="p-5 bg-secondary text-primary">
       <div>
         <div className="">
-          <h3 className="text-2xl font-semibold md:text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid quia quisquam qui at, alias aut voluptas? Eaque a nostrum possimus.</h3>
+          <h3 className="text-2xl font-semibold md:text-3xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            quia quisquam qui at, alias aut voluptas? Eaque a nostrum possimus.
+          </h3>
         </div>
         <div>
-          {caseStudies.map(study => (
-            <div key={study.id}>
-              
-            </div>
+          {caseStudies.map((study) => (
+            <Card
+              key={study.id}
+              headerImg={study.image}
+              cardTitle={study.title}
+              cardDescription={study.overview}
+              footer={{
+                buttonLabel: "View full Case study",
+                buttonLink: study.id,
+              }}
+            >
+              <div>
+                <span>Industry : ??? </span>
+                <p>Duration: <span>???</span></p>
+                <p>{study.impact}</p>
+              </div>
+            </Card>
           ))}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
