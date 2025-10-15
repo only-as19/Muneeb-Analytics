54484c88b7754c8f65ce59633a8e6be6a460273d
Asad <hellohello51981@gmail.com>
2025-10-15 23:22:00 +0500

    feat: Style and make it responsive

--- src/pages/CaseStudies/Sections/Title.tsx
+++ src/pages/CaseStudies/Sections/Title.tsx
@@ -5,14 +5,13 @@ const Title = () => {
     const {caseId} = useParams()
     const caseData = caseStudies.find(data=> data.id === caseId)
     return(
-    <section>
-        <div>
-            <div>
-                <h1>{caseData?.title}</h1>
-            <p>{caseData?.overview}</p>
-            </div>
-            <div>
-                <img src={caseData?.image} alt="" />
+    <section className="bg- min-h-96 p-5 flex flex-col gap-y-5 md:justify-between md:flex-row md:px-28 text-primary">
+        <div className="max-w-6xl mx-auto flex flex-col items-center md:flex-row gap-y-8 md:gap-x-12">
+            <img src={caseData?.image} alt={caseData?.title} className="w-80 md:w-100 md:order-2"/>
+            <div className="md:mr-10">
+                <h1 className="text-3xl md:text-4xl font-bold">{caseData?.title}</h1>
+            <p className="text-muted-foreground mt-2">{caseData?.overview}</p>
+            <p className="text-sm font-bold text-muted-foreground mt-1">Company: <span>{caseData?.company}</span></p>
             </div>
         </div>
     </section>
