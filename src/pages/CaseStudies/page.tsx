a0a4a7a4c503ac152f61af274dbb5550f3036eb7
Asad <hellohello51981@gmail.com>
2025-10-17 22:15:53 +0500

    feat: Added CTA

--- src/pages/CaseStudies/page.tsx
+++ src/pages/CaseStudies/page.tsx
@@ -1,11 +1,13 @@
 import { Title, Challanges, Solution } from "./Sections";
 
+import {CTA} from "@/components";
 const CaseStudy = () => {
   return (
     <main>
     <Title/>
     <Challanges/>
     <Solution/>
+    <CTA/>
   </main>
   )
   
