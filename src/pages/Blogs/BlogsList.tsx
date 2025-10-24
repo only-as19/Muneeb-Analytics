de59ffe10f182c0ccde0b8fb9e7a2990433f9c80
Asad <hellohello51981@gmail.com>
2025-10-24 21:12:11 +0500

    feat: render blogs title for checking that context is working properly

--- src/pages/Blogs/BlogsList.tsx
+++ src/pages/Blogs/BlogsList.tsx
@@ -1,7 +1,13 @@
+import { useBlogs } from "@/Hooks"
+
 const BlogsList:React.FC = () => {
+  const {blogs} = useBlogs()
+ console.log(blogs)
   return (
     <div>
-      
+      {blogs.map(blog=>(
+        <h1>{blog.title}</h1>
+      ))}
     </div>
   )
 }
