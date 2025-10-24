407100af70c6d8b122af7804176d6bab43859f7e
Asad <hellohello51981@gmail.com>
2025-10-24 23:39:38 +0500

    feat: slect categories function is added

--- src/pages/Blogs/BlogsList.tsx
+++ src/pages/Blogs/BlogsList.tsx
@@ -1,14 +1,24 @@
 import { useBlogs } from "@/Hooks";
 import Categories from "./categories";
+import { useState } from "react";
 const BlogsList: React.FC = () => {
   const { blogs, getCategories } = useBlogs();
+  const [category, setCategory] = useState("All");
   const categories = getCategories();
-  console.log(blogs)
-
+  console.log(category);
+  const filteredBlogs =
+    category === "All"
+      ? blogs
+      : blogs.filter((blog) => blog.category === category);
+  console.log(filteredBlogs);
   return (
     <div>
       <div className="p-5 max-w-7xl mx-auto relative -mt-8 z-50 pointer-events-auto">
-        <Categories categories={categories} />
+        <Categories
+          categories={categories}
+          setCategory={(cat) => setCategory(cat)}
+          selectedCategory={category}
+        />
       </div>
     </div>
   );
--- src/pages/Blogs/categories.tsx
+++ src/pages/Blogs/categories.tsx
@@ -2,18 +2,20 @@ import { Button } from "@/components";
 
 interface CategoryType {
   categories: string[];
+  setCategory: (cat:string)=> void;
+  selectedCategory:string;
 }
 
-const Categories: React.FC<CategoryType> = ({ categories }) => {
-  console.log(categories);
+const Categories: React.FC<CategoryType> = ({ categories, setCategory, selectedCategory }) => {
   return (
     <div className="flex flex-wrap gap-8 justify-center mb-16 bg-white/80  backdrop-blur-lg rounded-2xl shadow-xl p-6 border border-slate-200">
       {categories.map((category, index) => (
         <Button
           key={index}
           label={category}
-          className="md:p-5"
-          variant="outline"
+          className={`md:p-5 ${selectedCategory === category && ""}`}
+          variant={selectedCategory === category ? "default" : "outline"}
+          onClick={()=> setCategory(category)}
         />
       ))}
     </div>
