1a710ae7c64a4ccb8a0e92aa495274d565cd542d
Asad <hellohello51981@gmail.com>
2025-10-25 12:18:54 +0500

    feat: render feature blog

--- src/pages/Blogs/featureBlog.tsx
+++ src/pages/Blogs/featureBlog.tsx
@@ -1,19 +1,37 @@
+import { Button } from "@/components"
 import type { BlogData } from "./data"
-import { Card } from "@/components"
-
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/lib"
+import { DropdownMenuSeparator } from "@/lib"
+import { useNavigate } from "react-router"
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { MdDateRange,MdAccessTime } from "react-icons/md";
 
 interface featureBlogType {
     featureBlog: BlogData
 }
 
 const FeatureBlog:React.FC<featureBlogType> = ({featureBlog}) => {
+    const naviagte = useNavigate()
   return (
     <article>
-        <Card
-        headerImg={featureBlog.image}
-        cardTitle={featureBlog.title}
-        >
-
+        <Card>
+            <CardHeader>
+                <img src={featureBlog.image} alt={featureBlog.title} />
+            </CardHeader>
+            <CardContent>
+                <CardTitle>{featureBlog.title}</CardTitle>
+                <CardDescription>{featureBlog.description}</CardDescription>
+                <div>
+                    <p><MdDateRange/> <span>{featureBlog.date}</span></p>
+                    <p><MdAccessTime/> <span>{featureBlog.readingTime}</span></p>
+                </div>
+                <DropdownMenuSeparator/>
+                <CardFooter>
+                    <Button label="Read more" icon={<FaArrowLeftLong/>}
+                    iconDirection="left" variant="link"
+                    onClick={()=> naviagte(`blogs/${featureBlog.slug}`)}/>
+                </CardFooter>
+            </CardContent>
         </Card>
     </article>
   )
