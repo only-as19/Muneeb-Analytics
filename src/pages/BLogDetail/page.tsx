1e73314d8c8ece80960f45ae6b73c5ade9c3b18b
Asad <hellohello51981@gmail.com>
2025-10-26 11:00:38 +0500

    feat: Added Title data for Blog Deatil

--- src/pages/BLogDetail/page.tsx
+++ src/pages/BLogDetail/page.tsx
@@ -1,9 +1,28 @@
-const BlogDetail = () => {
-  return (
-    <div>
-      BlogDeatils
-    </div>
-  )
-}
+import { useParams } from "react-router-dom";
+import { useBlogs } from "@/Hooks";
+import { MdDateRange, MdAccessTime } from "react-icons/md";
 
-export default BlogDetail
+const BlogDetail: React.FC = () => {
+
+  const { slug } = useParams();
+  const { getBlogBySlug } = useBlogs();
+  const blog = slug ? getBlogBySlug(slug) : undefined;
+
+  return (
+    <main>
+        <div>
+            <div>
+                <span>{blog?.category}</span>
+                <h1>{blog?.title}</h1>
+                <p>{blog?.description}</p>
+                <div>
+                    <p><MdDateRange/> <span>{blog?.date}</span></p>
+                    <p><MdAccessTime/> <span>{blog?.readingTime}</span></p>
+                </div>
+            </div>
+        </div>
+    </main>
+  )
+};
+
+export default BlogDetail;
