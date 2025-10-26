a0c1575f0f72188ddc333385ce1ff0e6e8f0378d
Asad <hellohello51981@gmail.com>
2025-10-26 10:04:27 +0500

    feat: Return blogs using slug

--- src/Context/blogs_context.tsx
+++ src/Context/blogs_context.tsx
@@ -1,9 +1,10 @@
 import { blogs } from "@/pages/Blogs/data";
 import { createContext } from "react";
-
+import type { BlogData } from "@/pages/Blogs/data";
 interface BlogsContextType {
   blogs: typeof blogs;
   getCategories: () => string[];
+  getBlogBySlug: (slug: string) => BlogData | undefined
 }
 
 const BlogsContext = createContext<BlogsContextType | undefined>(undefined);
@@ -12,8 +13,13 @@ function BlogsProvider({ children }: { children: React.ReactNode }) {
   const getCategories = () => {
     return ["All", ...Array.from(new Set(blogs.map((blog) => blog.category)))];
   };
+
+  const getBlogBySlug = (slug: string) => {
+    return blogs.find((blog) => blog.slug === slug);
+  };
+
   return (
-    <BlogsContext.Provider value={{ blogs, getCategories }}>
+    <BlogsContext.Provider value={{ blogs, getCategories, getBlogBySlug }}>
       {children}
     </BlogsContext.Provider>
   );
