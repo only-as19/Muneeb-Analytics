62a094797c96096ef15edaa9b9946d8d13e52ccf
Asad <hellohello51981@gmail.com>
2025-10-29 10:59:34 +0500

    feat: create simple animation

--- src/Framer/motions.tsx
+++ src/Framer/motions.tsx
@@ -1,14 +1,23 @@
 import type { ReactNode } from "react"
-
+import { motion } from "motion/react"
 interface divProps{
 children:ReactNode
-width: "fit-content" | "100%"
+width?: "fit-content" | "100%"
+className?: string
 }
 
-const Motion:React.FC<divProps> = ({children, width = "fit-content"}) => {
+const Motion:React.FC<divProps> = ({children, width = "fit-content", className }) => {
   return (
-    <div className={`relative overflow-hidden ${width}`}>
-      <div>{children}</div>
+    <div className={`relative overflow-hidden ${width} ${className}`}>
+      <motion.div
+      variants={{
+        hidden:{opacity:0, y: 75},
+        visible:{opacity:1, y: 0}
+      }}
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 0.5, delay: 0.25 }}
+      >{children}</motion.div>
     </div>
   )
 }
--- src/pages/Landing/Sections/hero.tsx
+++ src/pages/Landing/Sections/hero.tsx
@@ -1,33 +1,52 @@
-import React from "react"
-import { hero_img } from "../data"
-import { Button } from "@/components"
-import { useNavigate } from "react-router"
+import React from "react";
+import { hero_img } from "../data";
+import { Button } from "@/components";
+import { useNavigate } from "react-router";
+import { Motion } from "@/Framer";
 
-const Hero:React.FC = () => {
-
-    const navigate = useNavigate()
+const Hero: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <section className="bg-bg-secondary min-h-96 p-5 flex flex-col gap-y-5 items-center md:justify-between md:flex-row md:px-28">
-        <img src={hero_img} alt="" 
-        className="w-100 md:min-w-120 rounded-xl drop-shadow-xl object-cover md:order-2 "
-        />
-        <div className="flex flex-col gap-y-4">
-            <h1 className="text-3xl font-bold text-primary tracking-tight md:text-6xl md:max-w-xl">Turn Your Data Into Decisions</h1>
-            <p className="text-muted-foreground text-sm max-w-xl">We help businesses unlock insights with interactive dashboards and predictive analytics, so your decisions are backed by data, not guesswork</p>
-            <div className="flex items-center justify-center space-x-3 md:justify-start">
-                <Button label="Request a Free Demo"
-                customClasses="md:py-5"
-                onClick={()=> navigate("/freeDemo")}
-                />
-                <Button label="Contact Us" 
-                variant="outline" 
-                customClasses="md:py-5"
-                onClick={()=> navigate("/contact")}
-                />
-            </div>
-        </div>
-    </section>
-  )
-}
+      <Motion className="md:order-2">
+        <img
+        src={hero_img}
+        alt=""
+        className="w-100 md:min-w-120 rounded-xl drop-shadow-xl object-cover"
+      />
+      </Motion>
+      <div className="flex flex-col gap-y-4">
+        <Motion>
+          <h1 className="text-3xl font-bold text-primary tracking-tight md:text-6xl md:max-w-xl">
+            Turn Your Data Into Decisions
+          </h1>
+        </Motion>
+        <Motion>
+          <p className="text-muted-foreground text-sm max-w-xl">
+            We help businesses unlock insights with interactive dashboards and
+            predictive analytics, so your decisions are backed by data, not
+            guesswork
+          </p>
+        </Motion>
 
-export default Hero
+        <Motion>
+          <div className="flex items-center justify-center space-x-3 md:justify-start">
+          <Button
+            label="Request a Free Demo"
+            customClasses="md:py-5"
+            onClick={() => navigate("/freeDemo")}
+          />
+          <Button
+            label="Contact Us"
+            variant="outline"
+            customClasses="md:py-5"
+            onClick={() => navigate("/contact")}
+          />
+        </div>
+        </Motion>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
