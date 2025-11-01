6f0d1b5eb490f2ab62b651d326a5c5d009f1a123
Asad <hellohello51981@gmail.com>
2025-11-01 22:54:13 +0500

    feat: Styled and make it responsive

--- src/pages/Testimonials/Sections/mainVideo.tsx
+++ src/pages/Testimonials/Sections/mainVideo.tsx
@@ -1,54 +1,54 @@
-import type {ClientProject} from "../data"
-import Video from "@/components/Video"
-import { Button } from "@/components"
-import { useNavigate } from "react-router"
+import type { ClientProject } from "../data";
+import Video from "@/components/Video";
+import { Button } from "@/components";
+import { useNavigate } from "react-router";
 import { CiShare1 } from "react-icons/ci";
 import { DropdownMenuSeparator } from "@/lib";
-import {Rating} from "@/components";
-interface VideoProps{
-    video: ClientProject
+import { Rating } from "@/components";
+interface VideoProps {
+  video: ClientProject;
 }
 
-const MainVideo:React.FC<VideoProps> = ({video}) => {
-  const navigate = useNavigate()
+const MainVideo: React.FC<VideoProps> = ({ video }) => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <div>
-        <Video
-      src={video.src}
-      aspectRatio="9:16"
-      />
-      </div>
-      <div>
-        <div></div>
-        <h3>Their Words</h3>
-        <h4>{video.clientName}</h4>
-        <span>{video.location}</span>
-        <div>
-          <span>{video.position}</span>
-        <Button 
-        label="percival pallets"
-        icon={<CiShare1/>}
-        iconDirection="right"
-        onClick={()=> navigate(`${video.website}`)}
-        className="cursor-pointer"
-        />
+    <div className="grid gap-y-4 md:grid-cols-5 md:gap-x-4">
+      <div className="relative md:col-span-3">
+        <div className="rounded-3xl overflow-hidden shadow-2xl bg-black ">
+          <Video src={video.src} />
         </div>
-        <p>My Role: <span>{video.myRole}</span></p>
-        <DropdownMenuSeparator/>
-        <div>
-          <div>
-            <span>Rating</span>
-            <Rating star={5}/>
+      </div>
+      <div className="bg-primary special-gradiant text-secondary p-5 rounded-3xl md:col-span-2">
+        <h3 className="text-xl font-bold">{video.clientName}</h3>
+        <span className="text-xs">{video.location}</span>
+        <div className="flex items-center">
+          <span className="font-semibold">{video.position}</span>
+          <Button
+            label="percival pallets"
+            icon={<CiShare1 strokeWidth={1} className="!w-5 !h-5"/>}
+            iconDirection="right"
+            onClick={() => navigate(`${video.website}`)}
+            className="text-secondary bg- uppercase cursor-pointer"
+            variant="link"
+          />
+        </div>
+        <p className="pb-4 text-lg text-center">
+          My Role: <span className="font-semibold">{video.myRole}</span>
+        </p>
+        <DropdownMenuSeparator />
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-1">
+            <span className="font-semibold">Rating</span>
+            <Rating star={5} />
           </div>
-          <div>
-            <span>Duration</span>
-            <span>{video.projectDuration}</span>
+          <div className="flex flex-col gap-y-1">
+            <span className="font-semibold">Duration</span>
+            <span className="font-semibold text-lg">{video.projectDuration}</span>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MainVideo
+export default MainVideo;
--- src/pages/Testimonials/Sections/testimonialsVideo.tsx
+++ src/pages/Testimonials/Sections/testimonialsVideo.tsx
@@ -3,13 +3,13 @@ import MainVideo from "./mainVideo";
 const TestimonailsVideos: React.FC = () => {
   const mainVideo = testimonialsVideos[0]
   return (
-    <section>
-      <div>
-        <h1>Our Success Stories</h1>
-        <p>
+    <section className="p-5 text-primary min-h-screen flex items-center flex-col justify-center">
+      <div className="max-w-6xl mx-auto">
+        <h1 className="text-3xl md:text-4xl font-bold text-center">Our Success Stories</h1>
+        <p className="text-muted-foreground mb-4 text-center">
           Discover the transformative impact we've had on leading businesses
         </p>
-        <div>
+        <div className="">
           <MainVideo video={mainVideo}/>
         </div>
       </div>
