8a4f7e0a919d03a2faf8e86bd02002b3e763c782
Asad <hellohello51981@gmail.com>
2025-11-01 14:16:53 +0500

    feat:

--- src/pages/Testimonials/Sections/Testimonials.tsx
+++ src/pages/Testimonials/Sections/Testimonials.tsx
@@ -17,6 +17,27 @@ const Testimonials: React.FC = () => {
           <TestimonialCard key={index} card={card}/>
         ))}
         </div>
+        <div className="hidden md:block">
+          <div className="marquee-row w-full mx-auto max-w-5xl overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-secondary to-transparent"></div>
+                <div className="marquee-inner flex transform-gpu min-w-[200%] pt-10 pb-5">
+                    {testimonialsData.map((card, index) => (
+                        <TestimonialCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-secondary to-transparent"></div>
+            </div>
+
+            <div className="marquee-row w-full mx-auto max-w-5xl overflow-hidden relative">
+                <div className="absolute left-0 top-0 h-full w-20 z-10 pointer-events-none bg-gradient-to-r from-secondaryto-transparent"></div>
+                <div className="marquee-inner marquee-reverse flex transform-gpu min-w-[200%] pt-10 pb-5">
+                    {testimonialsData.map((card, index) => (
+                        <TestimonialCard key={index} card={card} />
+                    ))}
+                </div>
+                <div className="absolute right-0 top-0 h-full w-20 md:w-40 z-10 pointer-events-none bg-gradient-to-l from-secondary to-transparent"></div>
+            </div>
+      </div>
       </div>
       </div>
     </section>
