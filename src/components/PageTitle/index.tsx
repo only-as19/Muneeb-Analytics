a954bdfa33043e888d819c6de0219cbf5c039b84
Asad <hellohello51981@gmail.com>
2025-12-09 22:12:14 +0500

    refactor: update title in contact

--- src/components/PageTitle/index.tsx
+++ src/components/PageTitle/index.tsx
@@ -13,7 +13,7 @@ interface TitleSectionProps{
 const BlogTitle:React.FC<TitleSectionProps> = ({heading,headingSpan,description,icon,slogan, direction = 'center'}) => {
   return (
     <div className={`special-gradiant ${direction === 'center' && "text-center"} md:py-12 px-5 md:px-8 py-10 font`}>
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-6xl mx-auto">
          {slogan && (
              <div className="inline-flex items-center gap-2 px-4 py-2 bg-emerald-500/20 backdrop-blur-sm border border-emerald-400/30 rounded-full mb-3">
               {icon && icon}
--- src/pages/Contact/page.tsx
+++ src/pages/Contact/page.tsx
@@ -1,13 +1,15 @@
 import {ContactForm,Info} from "./section"
 import { FAQ } from "@/components"
+import { PageTitle } from "@/components"
 
 const Contact = () => {
   return (
     <section className="bg-secondary">
-      <div className="bg-secondary flex flex-col justify-center items-center px-5 py-12 max-w-6xl mx-auto">
-        <h1 className="text-3xl md font-bold">Contact us</h1>
-        <p className="text-muted-foreground text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae consectetur cum dolores quod! Necessitatibus animi quos, eius accusantium eum vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, consequatur?</p>
-      </div>
+      <PageTitle slogan="Get In Touch With Me"
+      heading="Let's Build"
+      headingSpan="Something Great"
+      description="Whether you need to discuss your project, overcome a specific problem, or get consultation and expert support, I'm here to work alongside you. Your ambitions matter, send a message anytime, and we'll shape the results you envision."
+      />
       <ContactForm />
       <FAQ/>
       <Info/>
