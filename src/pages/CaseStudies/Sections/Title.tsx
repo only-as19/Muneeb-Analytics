bbb7536681dd95c7ec88f808bcd22f1c2eed296a
Asad <hellohello51981@gmail.com>
2026-03-03 16:08:23 +0500

    feat: changes made

--- src/pages/CaseStudies/Sections/Title.tsx
+++ src/pages/CaseStudies/Sections/Title.tsx
@@ -8,7 +8,7 @@ const Title:React.FC<TitlePtops> = ({introduction}) => {
     return(
     <section className="bg- min-h-96 p-5 flex flex-col gap-y-5 md:justify-between md:flex-row md:px-28 text-primary">
         <div className="max-w-6xl mx-auto flex flex-col items-center md:flex-row gap-y-8 md:gap-x-12">
-            <img src={introduction?.image} alt={introduction?.title} className="w-80 md:w-100 md:order-2"/>
+            <img src={introduction?.image} alt={introduction?.title} className="hidden lg:block w-80 md:w-100 md:order-2"/>
             <div className="md:mr-10">
                 <h1 className="text-3xl md:text-4xl font-bold">{introduction?.title}</h1>
             <p className="text-muted-foreground mt-2">{introduction?.overview}</p>
--- src/pages/Landing/Service Details/Sections/service-hero-section.tsx
+++ src/pages/Landing/Service Details/Sections/service-hero-section.tsx
@@ -10,7 +10,7 @@ const ServiceHero: React.FC<HeroType> = ({ heroData, image }) => {
   return (
     <section className="bg- min-h-96 p-5 flex flex-col gap-y-5 md:justify-between md:flex-row md:px-28 special-gradiant text-white">
         <div className="max-w-6xl mx-auto flex flex-col items-center md:flex-row gap-y-8 md:gap-x-12">
-            <img src={image} alt={heroData.title} className="w-80 md:w-100 md:order-2"/>
+            <img src={image} alt={heroData.title} className="hidden lg:block w-80 md:w-100 md:order-2"/>
             <div className="md:mr-10">
                 <h1 className="text-3xl md:text-5xl font-bold">{heroData?.title}</h1>
             <p className="text-gray-300 mt-2">{heroData.description}</p>
